d5238f04c3ffe18c0f815763aac0414aabec2d65
jeisonpantoja <jeisonpantoja@unicauca.edu.co>
2024-08-24 18:22:40 -0500

    added structure folder in webapp

--- webapp/src/App.tsx
+++ webapp/src/App.tsx
@@ -1,31 +1,6 @@
-import { FormEvent } from "react";
-
-function App() {
-  function startProccessing(event: FormEvent<HTMLFormElement>): void {
-    event.preventDefault();
-    console.log("Start processing");
-  }
-
+export const App = () => {
   return (
-    <div className="flex justify-center items-center p-4">
-      <div className="m-11 card h-full w-3/4 bg-slate-700">
-        <div className="p-4 md:p-10">
-          <h1>BiCo Chatbot</h1>
-          <h3 className="mt-6">Upload your documents</h3>
-          <form
-            action=""
-            method="post"
-            onSubmit={startProccessing}
-            className="w-full"
-          >
-            <div className="flex flex-col justify-center items-center">
-   
-            </div>
-          </form>
-        </div>
-      </div>
-    </div>
-  );
+    <div>App</div>
+  )
 }
 
-export default App;
